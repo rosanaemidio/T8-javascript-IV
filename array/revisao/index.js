d9fdfb384a2bad0d59d6bfc9478960ccff6bda83
AnaGFranco <anagsfranco@hotmail.com>
2019-11-04 13:17:34 -0300

    adiciona revisão da Aula1
    :wq

---
+++ array/revisao/index.js
@@ -0,0 +1,8 @@
+const comidas = ['Lasanha', 'Batata', 'Bacon']
+
+// console.log(comidas[2]);
+
+comidas.forEach((comida,posicao)=>{
+    console.log(`Na posição ${posicao} temos a ${comida}`);
+    
+})
