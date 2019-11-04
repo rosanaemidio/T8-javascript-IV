15b9748b725d527ff0236a456da133736ae676d4
rosanaemidio <rosanaemidio42@gmail.com>
2019-11-04 14:39:54 -0300

    resolve exercicio 01

--- array/ex1/index.js
+++ array/ex1/index.js
@@ -1,1 +1,20 @@
 let frutas = ["banana", "morango", "bacurí", "laranja"];
+let button = document.querySelector('#botao')
+let input = document.querySelector('#input')
+
+
+button.addEventListener("click", function( ){
+    let valorInput = input.value;
+    let listaFrutas = frutas.find((fruta) => fruta === valorInput);
+    
+    
+    if(listaFrutas){
+        console.log(listaFrutas)
+    }else {
+        alert("Esse item não está na lista!")
+    }
+    
+
+
+
+})
