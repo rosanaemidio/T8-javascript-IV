9516b945a430ae64d02d0069dc50fc58285de235
rosanaemidio <rosanaemidio42@gmail.com>
2019-11-05 12:40:11 -0300

    Adiciona o arquivo  de anotações

---
+++ objects/revisao/index.js
@@ -0,0 +1,14 @@
+function Professor(nome, idade, disciplina){
+    this.nome = nome
+    this.idade = idade
+    this.disciplina = disciplina
+
+}
+console.log(Professor);
+
+let obj1 = new Professor ("Juliana", 33, "Matemática" )
+console.log(obj1);
+
+let obj2 = new Professor ("Marta", 48, "Português")
+console.log(obj2);
+
